69ac91bb02502071588b9f36d376e2bfa13b84a9
rybalchenko <pavel3220@mail.ru>
2016-02-08 12:05:21 +0200

    first

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 ;(function($){
 
+	$(function(){
+		$('.ba-slider').slick({
+			dots: true,
+			infinite: false
+		});
+		var qtyField = $('.ba-product__qty-field'),
+		qtyUp = $('.ba-product__qty-up'),
+		qtyDown = $('.ba-product__qty-down');
+
+		qtyUp.on('click', function(event){
+			event.preventDefault();
+			var oldVal =  qtyField.val();
+
+			qtyField.val(++oldVal);
+		});
+
+		qtyDown.on('click', function(event){
+			event.preventDefault();
+			var oldVal =  qtyField.val();
+			if (--oldVal > 0){
+				qtyField.val(oldVal);
+			}
+		});
+		var prodColor = $('[name="product-color"]');
+		prodColor.on('change', function(){
+			// console.log($(this).val());
+
+		});
+
+	});
+
 })(jQuery);
